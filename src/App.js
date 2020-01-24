a4435737510a24c90e550766ada9909f7680ef87
Ajschlander <Aschlander21@gmail.com>
2020-01-24 10:31:37 -0600

    Added ball and bounds for paddle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Paddle from './components/Paddle';
+import Ball from './components/Ball';
 
 function App() {
   return (
     <div className="container">
       <Paddle />
+      <Ball />
     </div>
   );
 }
--- src/components/Paddle.js
+++ src/components/Paddle.js
@@ -1,17 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import "./Paddle.css";
 
-export default function Paddle({isPlayerTwo}){
-
-    let handleMouse = e => {
-        console.log(e.x, e.y, e.movementX, e.movementY)
+export default function Paddle() {
+  const [paddleX, setPaddleX] = useState(0);
+  function handleMouse(e) {
+    let boundedX;
+    const offset = (window.innerWidth - 500) / 2;
+    if (e.x - offset < 0) {
+      boundedX = 0;
+    } else if (e.x - offset > 350) {
+      boundedX = 350;
+    } else {
+      boundedX = e.x - offset;
     }
-
-    useEffect(() => {
-        window.addEventListener("mousemove", handleMouse)
-    })
-
-    return (
-        <div className="paddle"></div>
-    )
+    setPaddleX(boundedX);
+  }
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouse);
+  }, []);
+  return (
+    <div
+      className="paddle"
+      style={{
+        left: `${paddleX}px`
+        // transform: `translateX(calc(-50% + ${paddleX}px))`
+      }}
+    />
+  );
 }
